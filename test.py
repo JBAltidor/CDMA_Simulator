4b2376838f18df0eb2ad82ff882a13612e9f04de
Altidor <altidorjb@gmail.com>
2021-02-10 11:28:31 -0500

    This commit is made by Auto-Git 1, Wed 10 Feb 2021 11:28:29 AM EST

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-import numpy as np
+
 test =[1,2,3,34,6,7]
 test2=[2,3]
+test3 = [3,5,6,7]
+su = []
+for i in range (min (len(test),len(test2))):
+    su.append(test[i]+test2[i])
 
-array =np.array(test)
-array2=np.array(test2)
 
-print(array)
+print(su)

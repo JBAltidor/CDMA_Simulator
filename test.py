90a18ebcf9153789d233e0bdefc43ffb71f3f017
Altidor <altidorjb@gmail.com>
2021-02-10 11:34:20 -0500

    Auto-Push 2, Wed 10 Feb 2021 11:33:12 AM EST

--- test.py
+++ test.py
@@ -3,8 +3,10 @@ test =[1,2,3,34,6,7]
 test2=[2,3]
 test3 = [3,5,6,7]
 su = []
-for i in range (min (len(test),len(test2))):
+for i in range (max (len(test),len(test2),len (test3))):
     su.append(test[i]+test2[i])
 
+def AddVector(a ,b ):
+    
 
 print(su)

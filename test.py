22a7272769f61352e9bba94d7b9fc9df9eef2c9d
Altidor <altidorjb@gmail.com>
2021-02-10 11:41:51 -0500

    Auto-Push 9, Wed 10 Feb 2021 11:33:12 AM EST

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 
-test =[1,2,3,34,6,7]
-test2=[2,3]
-test3 = [3,5,6,7]
+test = [1, 2, 3, 34, 6, 7]
+test2 = [2, 3]
+test3 = [3, 5, 6, 7]
 
-def AddVector(a ,b ):
+
+def AddVector(a, b):
     added = []
-    for i in range (min (len(a),len(b))):
+    for i in range(min(len(a), len(b))):
         added.append(a[i]+b[i])
-    if (len(a)>len(b)):
-        added.append(a[i+1:])
-    elif (len(b)>len(a)):
-         added.append(b[i+1:])
+    if (len(a) > len(b)):
+        added = added + (a[i+1:]
+        # added.append(a[i+1:])
+    elif (len(b) > len(a)):
+        added=added + (b[i+1:]
+        #  added.append(b[i+1:])
     return added
 
 
-print(AddVector(test,test2))
+print(AddVector(test, test2))

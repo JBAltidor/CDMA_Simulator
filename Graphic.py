74a512029e38424adbf1634fdc250d000d67f860
Altidor <altidorjb@gmail.com>
2021-02-12 01:55:33 -0500

    Lazy Push

--- Graphic.py
+++ Graphic.py
@@ -76,7 +76,8 @@ def Start_simulation(nombre_users, bruit, msg_1, msg_2):
         #saving the lengths
         long1,long2=len(Encoded_Volt_1),len(Encoded_Volt_2)
         if (bruit == 1):
-            Traffic = cdma.Multiplexing(Encoded_Volt_1,Encoded_Volt_2,vo = cdma.Noise_Generator(max(len(Encoded_Volt_1),len(Encoded_Volt_2))))
+            vo = cdma.Noise_Generator(max(len(Encoded_Volt_1),len(Encoded_Volt_2)))
+            Traffic = cdma.Multiplexing(Encoded_Volt_1,Encoded_Volt_2,vo)
         else : Traffic = cdma.Multiplexing_2(Encoded_Volt_1,Encoded_Volt_2)
   
 

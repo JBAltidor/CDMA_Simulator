833d58dbc97a0030bec0f256d489fc25b98becc7
Altidor <altidorjb@gmail.com>
2021-02-17 14:05:18 -0500

    Lazy Push

--- draw.py
+++ draw.py
@@ -13,8 +13,8 @@ x = [1,2,3,4,5,6]
 y = [2,4,1,5,2,6] 
   
 # plotting the points  
-plt.plot(x, y, color='green', linestyle='dashed', linewidth = 3, 
-         marker='o', markerfacecolor='blue', markersize=6) 
+plt.plot(x, y, color='green',  linewidth = 3, 
+         marker='x', markerfacecolor='blue', markersize=6) 
   
 # setting x and y axis range 
 plt.ylim(1,8) 

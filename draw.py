142147ee7fc30287f535c76d4c8f249d3ec2eb8c
Altidor <altidorjb@gmail.com>
2021-02-17 19:21:02 -0500

    Adjusted range ,modified plotting

--- draw.py
+++ draw.py
@@ -1,24 +1,14 @@
-
-
-edit
-
-play_arrow
-
-brightness_4
 import matplotlib.pyplot as plt 
   
 # x axis values 
-x = [1,2,3,4,5,6] 
+x = [4288, 9000, 11168, 33528, 44720, 55904, 67080, 78256, 89432, 100608, 111784, 122960, 134152]
 # corresponding y axis values 
-y = [2,4,1,5,2,6] 
+y = [0, 0, 0, 2, 2, 1, 4, 0, 0, 5, 0, 0, 1128] 
   
 # plotting the points  
-plt.plot(x, y, color='green',  linewidth = 3, 
-         marker='x', markerfacecolor='blue', markersize=6) #linestyle='dashed'
-  
-# setting x and y axis range 
-plt.ylim(1,8) 
-plt.xlim(1,8) 
+plt.scatter(x, y, label= "stars", color= "green", marker= ".", s=30) 
+
+
   
 # naming the x axis 
 plt.xlabel('x - Nombre de bits envoyés') 
